059f77ebac1618d45daefadb6e0edaecf94417b8
Fran Gaspar <nectryk@gmail.com>
2025-07-22 19:57:21 +0200

    winform app created

---
+++ PersonalFinanceApp.WinForms/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PersonalFinanceApp.WinForms;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+
+        ServiceCollection services = new ();
+        ServicesConfiguration servicesConfiguration = new();
+        servicesConfiguration.ConfigureServices (services);
+
+        using (var serviceProvider = services.BuildServiceProvider())
+        {
+            var form = serviceProvider.GetService<Form1>();
+            Application.Run(form);
+        }
+    }
+}
---
+++ PersonalFinanceApp.WinForms/ServicesConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PersonalFinanceApp.Data;
+using PersonalFinanceApp.Services.Services;
+
+namespace PersonalFinanceApp.WinForms;
+
+internal class ServicesConfiguration
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<PersonalFinanceAppContext>(options =>
+        {
+            options.UseSqlite("Data Source=personalfinance.db");
+        });
+
+        services.AddScoped<IPaymentMethodService, PaymentMethodService>();
+        services.AddScoped<Form1>();
+    }
+}
